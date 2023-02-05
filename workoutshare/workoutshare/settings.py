4c0bd8824f772c8109f5449b172dd2f771db9f40
Digoin <titouan.dechanet@outlook.com>
2023-02-05 17:40:11 +0100

    admin staticsfiles wasn't found anymore so fixed it

--- workoutshare/workoutshare/settings.py
+++ workoutshare/workoutshare/settings.py
@@ -130,15 +130,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR /'static'
+STATIC_ROOT=BASE_DIR /'static'
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = '/static/'
 
 STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder',
 ]
 

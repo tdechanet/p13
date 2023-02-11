1bfa1f3196a533ea935d14eabba59a63ba5c48dc
Digoin <titouan.dechanet@outlook.com>
2023-02-12 00:56:42 +0100

    delete url

--- workoutshare/main/urls.py
+++ workoutshare/main/urls.py
@@ -7,4 +7,5 @@ APP_NAME = 'main'
 
 urlpatterns = [
     path('profile/', views.profile, name='profile'),
+    path('profile/<int:program_id>/delete/', views.delete_program, name='delete_program'),
 ]
